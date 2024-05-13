7029fdd31487a649e54430d07ffc0e7148c826ad
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-13 18:41:35 +0300

    Calculate frequency

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -17,3 +17,23 @@ head(bank_data)
 
 # View the dataset in a separate viewer window
 View(bank_data)
+
+# Calculate frequency of categorical variables
+frequency_location <- table(bank_data$location)
+frequency_merchant <- table(bank_data$merchant)
+frequency_gender <- table(bank_data$gender)
+frequency_fraud_label <- table(bank_data$fraud_label)
+
+# Print frequency tables
+print("Frequency of Location:")
+print(frequency_location)
+
+print("Frequency of Merchant:")
+print(frequency_merchant)
+
+print("Frequency of Gender:")
+print(frequency_gender)
+
+print("Frequency of Fraud Label:")
+print(frequency_fraud_label)
+
